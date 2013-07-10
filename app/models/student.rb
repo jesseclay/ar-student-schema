6b61058f0725d1b1be10fc14d0c1dba297a8af33
dbcapprentice <dbcapprentice@gmail.com>
2013-07-10 15:55:41 -0700

    created empty database

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,23 @@
 require_relative '../../db/config'
+require 'date'
 
 class Student < ActiveRecord::Base
-# implement your Student model here
+  attr_reader :first_name, :last_name, :gender, :dob, :email, :phone
+  def initialize(args)
+    @first_name = args.fetch(:first_name)
+    @last_name = args.fetch(:last_name)
+    @gender = args.fetch(:gender)
+    @DOB = args.fetch(:DOB)
+    @email = args.fetch(:email)
+    @phone = args.fetch(:phone)
+  end
+
+  def age
+    Date.today.strftime("%Y").to_i - @dob.scan(/\d{4}/).to_i
+  end
+
+  def name
+    @first_name + " " + @last_name
+  end
+
 end
